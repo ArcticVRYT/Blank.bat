9e628fc424039d99e0528bfa33941df009664576
ArcticVRYT <153539666+ArcticVRYT@users.noreply.github.com>
2024-03-01 20:46:51 -1000

    Add files via upload

---
+++ created.bat
@@ -0,0 +1,10 @@
+@echo off
+REM @echo off will make the cmd prompt to not be shown
+REM @echo on will make the cmd prompt to be shown
+
+REM you can do @echo on
+REM your code
+REM your code
+REM pause
+REM pause will make the cmd to be shown
+REM Thank you for using my app sincerely Flame
